02d5398411ad9c75111fdb9b64530908a0b2874d
kimsooho <kimsh9167@naver.com>
2021-05-17 17:32:32 +0900

    [Prg] API 호출 구조 확립

--- source/program/AutoTrading/AutoTrading/API/Accounts.cs
+++ source/program/AutoTrading/AutoTrading/API/Accounts.cs
@@ -12,23 +12,7 @@ namespace AutoTrading.API
         public Accounts(string accessKey, string secretKey) : base(accessKey, secretKey) { }
         public string GetAllAccounts()
         {
-            var payload = new JwtPayload
-        {
-            { "access_key", AccessKey },
-            { "nonce", Guid.NewGuid().ToString() },
-            //{ "query_hash", queryHash },
-            { "query_hash_alg", "SHA512" }
-        };
-
-            byte[] keyBytes = Encoding.Default.GetBytes(SecretKey);
-            var securityKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(keyBytes);
-            var credentials = new Microsoft.IdentityModel.Tokens.SigningCredentials(securityKey, "HS256");
-            var header = new JwtHeader(credentials);
-            var secToken = new JwtSecurityToken(header, payload);
-
-            var jwtToken = new JwtSecurityTokenHandler().WriteToken(secToken);
-            var authorizationToken = "Bearer " + jwtToken;
-            return CallRestAPI("GET", "https://api.upbit.com/v1/status/wallet", authorizationToken);
+            return CallRestAPI("GET", "https://api.upbit.com/v1/market/all?isDetails=false", MakeTocken());
         }
     }
 }
--- source/program/AutoTrading/AutoTrading/API/RestApiBase.cs
+++ source/program/AutoTrading/AutoTrading/API/RestApiBase.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,6 +20,45 @@ namespace AutoTrading.API
             AccessKey = accessKey;
             SecretKey = secretKey;
         }
+        /// <summary>
+        /// API에 파라미터가 필요한 경우, Dic에 key, value로 넣어서 함수 호출하셈.
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        protected string MakeTocken(Dictionary<string, string> param = null)
+        {
+            if (param is null) param = new Dictionary<string, string>();
+
+            StringBuilder builder = new StringBuilder();
+            foreach (KeyValuePair<string, string> pair in param)
+            {
+                builder.Append(pair.Key).Append("=").Append(pair.Value).Append("&");
+            }
+            string queryString = builder.ToString().TrimEnd('&');
+
+            SHA512 sha512 = SHA512.Create();
+            byte[] queryHashByteArray = sha512.ComputeHash(Encoding.UTF8.GetBytes(queryString));
+            string queryHash = BitConverter.ToString(queryHashByteArray).Replace("-", "").ToLower();
+
+            var payload = new JwtPayload
+            {
+                { "access_key", AccessKey },
+                { "nonce", Guid.NewGuid().ToString() },
+                { "query_hash", queryHash },
+                { "query_hash_alg", "SHA512" }
+            };
+
+            byte[] keyBytes = Encoding.Default.GetBytes(SecretKey);
+            var securityKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(keyBytes);
+            var credentials = new Microsoft.IdentityModel.Tokens.SigningCredentials(securityKey, "HS256");
+            var header = new JwtHeader(credentials);
+            var secToken = new JwtSecurityToken(header, payload);
+
+            var jwtToken = new JwtSecurityTokenHandler().WriteToken(secToken);
+            var authorizationToken = "Bearer " + jwtToken;
+
+            return authorizationToken;
+        }
 
         protected string CallRestAPI(string methodType, string URL, string tocken)
         {
@@ -25,7 +66,6 @@ namespace AutoTrading.API
 
             try
             {
-
                 WebRequest request = WebRequest.Create(URL);
                 request.Method = methodType;
                 request.ContentType = "application/json";
