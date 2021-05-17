3c00c37bd8449b541bb47ee639b93bef6d56d4fa
kimsooho <kimsh9167@naver.com>
2021-05-17 14:20:40 +0900

    [Prg] API base 작업 중, 프로젝트 정보 갱신

---
+++ source/program/AutoTrading/AutoTrading/API/RestApiBase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTrading.API
+{
+    class RestApiBase
+    {
+        protected string AccessKey;
+        protected string SecretKey;
+
+        protected RestApiBase(string accessKey, string secretKey)
+        {
+            AccessKey = accessKey;
+            SecretKey = secretKey;
+        }
+
+        protected string CallRestAPI(string methodType, string URL, string tocken)
+        {
+            string responseFromServer = string.Empty;
+
+            try
+            {
+
+                WebRequest request = WebRequest.Create(URL);
+                request.Method = methodType;
+                request.ContentType = "application/json";
+                request.Headers.Add(string.Format("Authorization:{0}", tocken));
+
+                using (WebResponse response = request.GetResponse())
+                using (Stream dataStream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(dataStream))
+                {
+                    responseFromServer = reader.ReadToEnd();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            return responseFromServer;
+        }
+    }
+}
---
+++ source/program/AutoTrading/AutoTrading/API/RestApiManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTrading.API
+{
+    class RestApiManager
+    {
+        protected static RestApiManager Instance = null;
+
+        private Accounts Accounts;
+        /// <summary>
+        /// Singleton
+        /// </summary>          
+        public static RestApiManager GetInstance()
+        {
+            if(Instance is null)
+            {
+                Instance = new RestApiManager();
+            }
+
+            return Instance;
+        }
+
+        public void SetKeys(string accessKey, string secretKey)
+        {
+            Accounts = new Accounts(accessKey, secretKey);
+        }
+
+        public Accounts GetAccounts() { return Accounts; }
+    }
+}
