1a71a99c491d3d963326cb1c9054a089f00fe668
kimsooho <kimsh9167@naver.com>
2021-07-13 03:27:59 +0900

    [Win] Init Column Base

--- source/program/AutoTrading/AutoTrading/UserControls/Accounts/UCAccountList.cs
+++ source/program/AutoTrading/AutoTrading/UserControls/Accounts/UCAccountList.cs
@@ -19,12 +19,42 @@ namespace AutoTrading.UserControls.Accounts
         {
             InitializeComponent();
             InitEvents();
+            InitColumns();
         }
 
         private void InitEvents()
         {
             Debug.Log(RestApiManager.GetInstance().GetAccounts().GetAllAccounts());
             
+        }
+
+        private void FillData()
+        {
+            //DataTable 
+        }
+
+        private void InitColumns()
+        {
+            List<Column> listColumn = new List<Column>
+            {
+                new Column("currency", "화폐", true),
+                new Column("balance", "주문 가능 금액", true),
+                new Column("locked", "주문 중인 금액", true),
+                new Column("avg_buy_price", "매수 평균가", true),
+                new Column("avg_buy_price_modified", "매수 평균가 수정 여부", true),
+                new Column("unit_currency", "평단가 기준", true)
+            };
+
+            DataTable dt = new DataTable();
+            gvCoin.DataSource = dt;
+
+            foreach (Column col in listColumn)
+            {
+                dt.Columns.Add(col.Name);
+                dt.Columns[col.Name].Caption = col.Caption;
+                gvCoin.Columns[col.Name].Visible = col.Visible;
+                gvCoin.Columns[col.Name].HeaderText = col.Caption;                
+            }
         }
     }
 }
---
+++ source/program/AutoTrading/AutoTrading/Util/Column.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTrading.Util
+{
+    class Column
+    {
+        public string Name { get; set; }
+        public string Caption { get; set; }
+        public bool Visible { get; set; }
+
+        public Column(string name, string caption, bool visible = true)
+        {
+            this.Name = name;
+            this.Caption = caption;
+            this.Visible = visible;
+        }
+    }
+}
